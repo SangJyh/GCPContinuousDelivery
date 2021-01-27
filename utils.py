00417045d688fdf12fe474607ab14c521b0c4a55
Joe Krinke <joekrinke15@gmail.com>
2021-01-27 16:13:37 +0000

    Add beginnings of style transfer

---
+++ utils.py
@@ -0,0 +1,32 @@
+import os
+import tensorflow as tf
+"""
+Inspiration: https://www.tensorflow.org/lite/models/style_transfer/overview
+"""
+
+# Load models 
+style_predict_path = tf.keras.utils.get_file('style_predict.tflite', 'https://tfhub.dev/google/lite-model/magenta/arbitrary-image-stylization-v1-256/int8/prediction/1?lite-format=tflite')
+style_transform_path = tf.keras.utils.get_file('style_transform.tflite', 'https://tfhub.dev/google/lite-model/magenta/arbitrary-image-stylization-v1-256/int8/transfer/1?lite-format=tflite')
+
+# Function to load an image from a file, and add a batch dimension.
+def load_img(path_to_img):
+  img = tf.io.read_file(path_to_img)
+  img = tf.io.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  img = img[tf.newaxis, :]
+
+  return img
+
+# Function to pre-process by resizing an central cropping it.
+def preprocess_image(image, target_dim):
+  # Resize the image so that the shorter dimension becomes 256px.
+  shape = tf.cast(tf.shape(image)[1:-1], tf.float32)
+  short_dim = min(shape)
+  scale = target_dim / short_dim
+  new_shape = tf.cast(shape * scale, tf.int32)
+  image = tf.image.resize(image, new_shape)
+
+  # Central crop the image.
+  image = tf.image.resize_with_crop_or_pad(image, target_dim, target_dim)
+
+  return image
