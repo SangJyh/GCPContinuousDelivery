fbb714ffa3e873ba3d61344adba267eb47fb5168
Joe Krinke <joekrinke15@gmail.com>
2021-01-27 01:15:46 +0000

    Fixed functionality to provide random dog images

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import requests
 
 app = Flask(__name__)
 @app.route('/')
-def mainpage():
-    endpoint="https://dog.ceo/api/breeds/image/random";
+def main():
+    endpoint = "https://dog.ceo/api/breeds/image/random"
     response = requests.get(endpoint)
-    return(response)
+    img_url = response.json()['message']
+    image_html = """<img src = """ + img_url + """ alt = "Random Basenji Image">"""
+    return (image_html)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
